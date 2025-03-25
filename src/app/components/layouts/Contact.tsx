9f814d60142143845040954e0567b8ff98017e99
bq22005 <bq22005@shibaura-it.ac.jp>
2025-03-25 18:20:33 +0900

    Update infomation layout

---
+++ src/app/components/layouts/Contact.tsx
@@ -0,0 +1,12 @@
+import styles from "./Contact.module.css";
+import { Title } from "@/app/components/elements/Title";
+
+export function Contact() {
+  return (
+    <div className={styles.contact}>
+      <Title>Contact</Title>
+      <p className={styles.text}>Mail: bq22005@shibaura-it.ac.jp</p>
+      <p className={styles.text}>GitHub: https://github.com/bq22005</p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import styles from "./page.module.css";
 import { Profile } from "@/app/components/layouts/Profile";
 import { About } from "./components/layouts/About";
 import { Works } from "@/app/components/layouts/Works";
+import { Contact } from "./components/layouts/Contact";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
         <div className={styles.infomation}>
           <About />
           <Works />
+          <Contact />
         </div>
       </div>
     </div>
