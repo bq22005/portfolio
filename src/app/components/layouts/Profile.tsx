3bec9d7c8b516b65f76c172e826913f2a20945f8
bq22005 <bq22005@shibaura-it.ac.jp>
2025-03-25 17:16:36 +0900

    Update profile layout

--- src/app/components/layouts/Profile.tsx
+++ src/app/components/layouts/Profile.tsx
@@ -1,12 +1,19 @@
 import styles from "./Profile.module.css";
-import Image from "next/image";
 
 export function Profile() {
   return (
     <div className={styles.profile}>
-      <h2 className={styles.sentence}>Hi, There!</h2>
-      <div className={styles.iconContainer}>
-        <img className={styles.icon} src="/deer-icon.png" alt="profile-icon" />
+      <div className={styles.greetingContainer}>
+        <h2 className={styles.greeting}>Hi, There!</h2>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.text}>
+          <p>Rikuto Kanzaki</p>
+          <p>Affiliation: Shibaura Inst. of Tech.</p>
+        </div>
+        <div className={styles.iconContainer}>
+          <img className={styles.icon} src="/deer-icon.png" alt="profile-icon" />
+        </div>
       </div>
     </div>
   );
