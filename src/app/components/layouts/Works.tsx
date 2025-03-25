45a8d7c6ebdb03aba7a3d0ea8fcbe8cb7a1c21b5
bq22005 <bq22005@shibaura-it.ac.jp>
2025-03-25 20:49:41 +0900

    Add carousel to works

--- src/app/components/layouts/Works.tsx
+++ src/app/components/layouts/Works.tsx
@@ -1,20 +1,66 @@
 import styles from "./Works.module.css";
 import { Title } from "@/app/components/elements/Title";
-import { WorkItem } from "./WorkItem";
+import { WorkItem } from "@/app/components/layouts/WorkItem";
 
 export function Works() {
   return (
     <div className={styles.works}>
       <Title>Works</Title>
-      <div className={styles.container}>
-        <WorkItem
-          imgSrc="/SalTrac-icon.png"
-          imgAlt="saltrac-icon"
-          explanation="　ハッカソンにて、SNS 機能と塩分摂取量管理を組み合わせたSalTracというWebアプリケーションを製作しました。"
-          link="https://github.com/Dreams20250215/SalTrac.git"
-        >
-          SalTrac
-        </WorkItem>
+      <div className={styles.carousel}>
+        <div className={styles.wrapper}>
+          <div className={styles.group}>
+            <WorkItem
+              imgSrc="/SalTrac-icon.png"
+              imgAlt="saltrac-icon"
+              explanation="　ハッカソンにて、SNS 機能と機械学習による塩分摂取量管理を組み合わせたWebアプリケーションを製作しました。"
+              link="https://github.com/Dreams20250215/SalTrac.git"
+            >
+              SalTrac
+            </WorkItem>
+            <WorkItem
+              imgSrc="/SalTrac-icon.png"
+              imgAlt="saltrac-icon"
+              explanation="　ハッカソンにて、SNS 機能と機械学習による塩分摂取量管理を組み合わせたWebアプリケーションを製作しました。"
+              link="https://github.com/Dreams20250215/SalTrac.git"
+            >
+              SalTrac
+            </WorkItem>
+            <WorkItem
+              imgSrc="/SalTrac-icon.png"
+              imgAlt="saltrac-icon"
+              explanation="　ハッカソンにて、SNS 機能と機械学習による塩分摂取量管理を組み合わせたWebアプリケーションを製作しました。"
+              link="https://github.com/Dreams20250215/SalTrac.git"
+            >
+              SalTrac
+            </WorkItem>
+          </div>
+          <div className={styles.group}>
+            <WorkItem
+              imgSrc="/SalTrac-icon.png"
+              imgAlt="saltrac-icon"
+              explanation="　ハッカソンにて、SNS 機能と機械学習による塩分摂取量管理を組み合わせたWebアプリケーションを製作しました。"
+              link="https://github.com/Dreams20250215/SalTrac.git"
+            >
+              SalTrac
+            </WorkItem>
+            <WorkItem
+              imgSrc="/SalTrac-icon.png"
+              imgAlt="saltrac-icon"
+              explanation="　ハッカソンにて、SNS 機能と機械学習による塩分摂取量管理を組み合わせたWebアプリケーションを製作しました。"
+              link="https://github.com/Dreams20250215/SalTrac.git"
+            >
+              SalTrac
+            </WorkItem>
+            <WorkItem
+              imgSrc="/SalTrac-icon.png"
+              imgAlt="saltrac-icon"
+              explanation="　ハッカソンにて、SNS 機能と機械学習による塩分摂取量管理を組み合わせたWebアプリケーションを製作しました。"
+              link="https://github.com/Dreams20250215/SalTrac.git"
+            >
+              SalTrac
+            </WorkItem>
+          </div>
+        </div>
       </div>
     </div>
   );
