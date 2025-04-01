7106534bf5250c1df48c9d4ea807713664d9e3bf
bq22005 <bq22005@shibaura-it.ac.jp>
2025-04-01 15:54:59 +0900

    Update item species

--- src/app/components/layouts/Works.tsx
+++ src/app/components/layouts/Works.tsx
@@ -19,12 +19,12 @@ export function Works() {
                 SalTrac
               </WorkItem>
               <WorkItem
-                imgSrc="/SalTrac-icon.png"
-                imgAlt="saltrac-icon"
-                explanation="　ハッカソンにて、SNS 機能と機械学習による塩分摂取量管理を組み合わせたWebアプリケーションを製作しました。"
+                imgSrc="/willtify-icon.png"
+                imgAlt="willtify-icon"
+                explanation="　宛先を指定せず、送信日のみを指定して手紙を送信するWebアプリケーションを製作しています。"
                 link="https://github.com/Dreams20250215/SalTrac.git"
               >
-                SalTrac
+                willtify
               </WorkItem>
               <WorkItem
                 imgSrc="/SalTrac-icon.png"
@@ -45,12 +45,12 @@ export function Works() {
                 SalTrac
               </WorkItem>
               <WorkItem
-                imgSrc="/SalTrac-icon.png"
-                imgAlt="saltrac-icon"
-                explanation="　ハッカソンにて、SNS 機能と機械学習による塩分摂取量管理を組み合わせたWebアプリケーションを製作しました。"
+                imgSrc="/willtify-icon.png"
+                imgAlt="willtify-icon"
+                explanation="　宛先を指定せず、送信日のみを指定して手紙を送信するWebアプリケーションを製作しています。"
                 link="https://github.com/Dreams20250215/SalTrac.git"
               >
-                SalTrac
+                willtify
               </WorkItem>
               <WorkItem
                 imgSrc="/SalTrac-icon.png"
